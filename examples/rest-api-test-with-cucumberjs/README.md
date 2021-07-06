9ebb6da844aa356cd77428d17fbbc8daf0f350a3
Pyunghyuk Yoo <yoophi@gmail.com>
2021-07-06 20:56:45 +0900

    ✨ NEW: rest-api test with Cucumber.js

---
+++ examples/rest-api-test-with-cucumberjs/README.md
@@ -0,0 +1,14 @@
+# Example with Cucumber.js
+
+
+## Run
+
+```
+$ npm i
+$ npx cucumber-js -f json:cucumber.json src/features/ -r src/steps/ --tags '@directory-service'
+```
+
+## Reference
+
+<https://dev.to/ynmanware/bdd-for-rest-api-using-cucumber-js-2pol>
+
---
+++ examples/rest-api-test-with-cucumberjs/src/steps/world.js
@@ -0,0 +1,23 @@
+
+const {setWorldConstructor} = require("@cucumber/cucumber");
+
+if (!process.env.SERVICE_URL) {
+    require('dotenv-flow').config();
+}
+
+class CustomWorld {
+    constructor({parameters}) {
+        this.context = {};
+        this.variable = 0;
+    }
+
+    setTo(number) {
+        this.variable = number;
+    }
+
+    incrementBy(number) {
+        this.variable += number;
+    }
+}
+
+setWorldConstructor(CustomWorld);
